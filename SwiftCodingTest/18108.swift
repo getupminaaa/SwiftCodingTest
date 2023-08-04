3d3d2c69987b229f8d5fe9f7ed59b57b8322705b
getupminaaa <getupmina@gmail.com>
2023-08-04 15:42:55 +0900

    Day 3 10926, 18108 solve

---
+++ SwiftCodingTest/18108.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftCodingTest
+//
+//  Created by getupmina on 2023/08/04.
+//
+
+import Foundation
+
+let year = Int(readLine()!)!
+
+print(year - 543)
---
+++ SwiftCodingTest/SwiftCodingTest/10926.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  SwiftCodingTest
+//
+//  Created by getupmina on 2023/08/04.
+//
+
+import Foundation
+let input = readLine()!
+//방법 1
+print(input+"??!")
+//방법 2
+print(input,"??!",separator: "")
